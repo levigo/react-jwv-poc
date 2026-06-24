037e63b90428cdc49d9963e217dbf42f2d51b98d
Xenia Wetzel <x.wetzel@levigo.de>
2026-06-24 16:14:34 +0200

    fix(JWT-4687): fix styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './theme.scss';
 import App from './App';
 import {preloadPrecursor} from "@levigo/webtoolkit-ng-client";
 import '@levigo/jadice-web-icons/assets/icon-font.scss';
